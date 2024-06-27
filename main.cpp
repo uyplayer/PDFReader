340406276c5d550f96c09d44554a433ac85c16d9
uyplayer <32848979@qq.com>
2024-06-27 20:34:36 +0800

    test sha

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include "src/mainWindow/mainwindow.h"
 #include <iostream>
+#include <sha.h>
+#include <hex.h>
+#include <filters.h>
 
 
 int main(int argc, char* argv[])
@@ -8,6 +11,13 @@ int main(int argc, char* argv[])
 
     std::cout << "Hello PDFGlimpse !" << std::endl;
     std::cout << "Project dir "<< PROJECT_DIR   << std::endl;
+
+    std::string message = "Hello, PDFGlimpse!";
+    std::string digest;
+    CryptoPP::SHA256 hash;
+    CryptoPP::StringSource(message, true, new CryptoPP::HashFilter(hash, new CryptoPP::HexEncoder(new CryptoPP::StringSink(digest))));
+    std::cout << "Message: " << message << std::endl;
+    std::cout << "SHA-256 Digest: " << digest << std::endl;
 
     QApplication app(argc, argv);
     mainWindow mainWindow;
